e842795867e7542c31c9cccdba3042fd33baba00
sambhav0000 <134165425+sambhav0000@users.noreply.github.com>
2025-01-15 18:06:46 +0530

    Add files via upload

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+module.exports = withMT({
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./subcomponents/**/*.{js,ts,jsx,tsx}",
+    "./layouts/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      primary: {
+        // DEFAULT: "#76528BFF",
+        DEFAULT: "#5F4B8BFF",
+      }
+    },
+  },
+  plugins: [],
+});
